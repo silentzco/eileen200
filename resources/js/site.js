70eec9430bf9eebfdc83c13cd84638db45c1a7ae
Brian Hughes <brian@cleanprogram.com>
2021-01-24 23:52:15 -0500

    Initial add of Algolia libraries

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,1 +1,23 @@
 // This is all you.
+import algoliasearch from 'algoliasearch/lite';
+import instantsearch from 'instantsearch.js';
+import { searchBox, hits } from 'instantsearch.js/es/widgets';
+
+const searchClient = algoliasearch('LYOAOTOT4D', '06f6868f86dd05c03ba16ec7f56db53d');
+
+const search = instantsearch({
+    indexName: 'providers',
+    searchClient,
+});
+
+search.addWidgets([
+    searchBox({
+        container: "#searchbox"
+    }),
+
+    hits({
+        container: "#hits"
+    })
+]);
+
+search.start();
